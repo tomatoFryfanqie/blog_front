be4d062c9401814d6e3878fce19825f61163ca69
lzh <1093369951@qq.com>
2021-10-11 22:23:47 +0800

    首次提交

---
+++ src/views/Login.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <LoginComponent msg="Hello" />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+//import HelloWorld from "@/components/HelloWorld.vue";
+import LoginComponent from "../components/LoginComponent.vue";
+
+export default {
+  name: "Login",
+  components: {
+    LoginComponent,
+  },
+};
+</script>
---
+++ src/views/Register.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <RegisterComponent msg="Vue" />
+  </div>
+</template>
+
+<script>
+import RegisterComponent from "../components/RegisterComponent.vue"
+  export default{
+    name : "Register",
+    components : {
+      RegisterComponent
+    }
+  }
+</script>
